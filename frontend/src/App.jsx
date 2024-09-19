4ba387db45cffd75b9513143d544c7f19480a38e
rusgyn <rusgynmagsanay@gmail.com>
2024-09-19 17:03:57 -0400

    Use mock data and populate the photos and topics.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import HomeRoute from 'routes/HomeRoute';
+import photos from "./mocks/photos"
+import topics from "./mocks/topics"
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -8,7 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute />    
+      <HomeRoute photo={photos} topic={topics}/>    
     </div>
   );
 };
