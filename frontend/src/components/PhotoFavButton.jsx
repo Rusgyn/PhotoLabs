7df766f4554c46f704f23b0fb1fc84b010f87e8a
rusgyn <rusgynmagsanay@gmail.com>
2024-09-20 16:04:28 -0400

    Clean-up. Remove commented out lines of code.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,34 +5,15 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
 
-  // const [isFave, setIsFave] = useState(false);
   const { onClick, isFavorites } = props
 
-  // const addToFavorites = () => {
-  //   setFavorites((prev) => [...prev, identifier])
-  // };
-
-  // const removeFromFavorites = () => {
-  //   setFavorites((prev) => prev.filter((fave) => fave !== identifier));
-  // };
-
-  // const faveIconToggle = () => {
-  //   setIsFave((prevIsFave) => {
-  //     const currentToggle = !prevIsFave;
-  //     if (currentToggle) {
-  //       addToFavorites();
-  //     } else {
-  //       removeFromFavorites();
-  //     }
-  //     return currentToggle; // ensures that we are working with the most recent state.
-  //   })
-  // };
-
-//onClick={faveIconToggle} 
   return (
     <div className="photo-list__fav-icon" >
       <div className="photo-list__fav-icon-svg">
-        <FavIcon onClick={onClick} selected={isFavorites} />
+        <FavIcon 
+          onClick={onClick}
+          selected={isFavorites}
+        />
       </div>
     </div>
   );
