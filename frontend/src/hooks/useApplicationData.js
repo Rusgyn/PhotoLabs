f861761d65ae8e354207c3f1a9a12626af77d891
rusgyn <rusgynmagsanay@gmail.com>
2024-09-22 09:49:03 -0400

    Create the reducer by removing the useState and replace it with useReducer.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,41 +1,67 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
-const useApplicationData = () => {
-
-  const [favorites, setFavorites] = useState([]); // Add photo to favorites
-  const [inFavorites, setInFavorites] = useState([])
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState({
-    id: undefined,
-    location: undefined,
-    similar_photos: undefined,
-    urls: undefined,
-    user: undefined
-  });
-
-  const toggleModal = () => {
-    setIsModalOpen(prevIsModal => !prevIsModal);
-  };
-  
-  const toggleAddToFavorites = (id, inFavorites) => {
-    setFavorites((prevFavorites) =>
-      inFavorites ? prevFavorites.filter((photoId) => photoId !== id) : [...prevFavorites, id]
-    );
-  };
-
-  return ({
-    favorites,
-    setFavorites,
-    inFavorites,
-    setInFavorites,
-    isModalOpen,
-    setIsModalOpen,
-    selectedPhoto,
-    setSelectedPhoto,
-    toggleModal,
-    toggleAddToFavorites
-  });
-
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
 }
 
-export default useApplicationData;
+function reducer(state, action) {
+  switch (action.type) {
+    case FAV_PHOTO_ADDED:
+      return { ...state,
+        favorites: [...state.favorites, action.payload.id]
+    }
+    case FAV_PHOTO_REMOVED:
+      return { ...state,
+        favorites: state.favorites.filter((photoId) => photoId !== action.payload.id)
+      }
+    case SELECT_PHOTO:
+      return { ...state, 
+        selectedPhoto: action.payload.id
+      }
+    case DISPLAY_PHOTO_DETAILS:
+      return { ...state,
+        isModalOpen: true
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+function useApplicationData() {
+  const initialState = {
+    favorites: [],
+    isModalOpen: false,
+    selectedPhoto: {
+      id: undefined,
+      location: undefined,
+      similar_photos: undefined,
+      urls: undefined,
+      user: undefined
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleModal = () => {
+    dispatch({type: 'DISPLAY_PHOTO_DETAILS'});
+  };
+
+  const toggleAddToFavorites = (id, inFavorites) => {
+    (inFavorites && dispatch({type: 'FAV_PHOTO_REMOVED', payload: {id}}));
+    (!inFavorites && dispatch({type: 'FAV_PHOTO_ADDED', payload: {id}}));
+  };
+
+  return {
+    ...state,
+    toggleModal,
+    toggleAddToFavorites
+  };
+
+};
